7635c4b51de3b4b9c2ff566994b19f5d857e926b
Lenisa Marisco <162896124+lenisamarisco@users.noreply.github.com>
2024-08-01 12:34:29 -0400

    let practice

---
+++ serial.py
@@ -0,0 +1,45 @@
+"""Python serial number generator."""
+
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+    
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+
+    def __init__(self, start=0):
+        self.start = self.next = start
+
+    def __repr__(self):
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+
+    def generate(self):
+        """Return next serial."""
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        """Reset number to original start."""
+        self.next = self.start
+
+# Testing the SerialGenerator class
+if __name__ == "__main__":
+    serial = SerialGenerator(start=100)
+    print(serial.generate())  # Expected output: 100
+    print(serial.generate())  # Expected output: 101
+    print(serial.generate())  # Expected output: 102
+    serial.reset()
+    print(serial.generate())  # Expected output: 100
